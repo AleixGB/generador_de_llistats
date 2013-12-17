7e8afb9360a78839b7fc4dca286b85a7feef4802
Aleix Guix <Aleix@172.24.1.47>
2013-12-17 16:10:36 +0100

    Classe LectorCSV creada

---
+++ src/generadorDeLlistats/CVS.java
@@ -0,0 +1,45 @@
+package generadorDeLlistats;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CVS {
+    
+    CVS obj = new CVS();
+    //obj.run();
+    
+    public void run() {
+
+        String csvFile = "./src/arxiu.csv"; // cuidado amb el nom
+        BufferedReader buffer = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+
+            buffer = new BufferedReader(new FileReader(csvFile));
+            while ((line = buffer.readLine()) != null) {
+                String[] csv = line.split(cvsSplitBy);
+                System.out.println("Alumne = Nom: " + csv[1] + csv[2]);
+            }
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (buffer != null) {
+                try {
+                    buffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("Ok");
+    }
+
+    
+}
